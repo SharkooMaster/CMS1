f3853597d83642602714702b60f5df8b004e4cf2
Sharkoo <alirashini@hotmail.com>
2018-11-02 17:10:30 +0100

    Added Hybrid launch systems.

--- CMS1/Program.cs
+++ CMS1/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
 
 namespace CMS1
 {
@@ -12,13 +16,12 @@ namespace CMS1
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
 
-        //gud my man
     }
 }
